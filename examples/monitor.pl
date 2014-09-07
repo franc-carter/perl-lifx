00b0209f7c95afa346570680a90177b2865f0009
Franc Carter <franc@darthcarter.com>
2014-09-07 22:43:53 +1000

    monitor example prints mac addresses

--- examples/monitor.pl
+++ examples/monitor.pl
@@ -8,6 +8,14 @@ use Data::Dumper;
 
 my $lifx = Device::LIFX->new();
 
+sub mac2str($)
+{
+    my @mac = unpack('C6',$_[0]);
+    @mac    = map {sprintf("%02x",$_)} @mac;
+
+    return join(":", @mac);
+}
+
 my $bulb = undef;
 while(1) {
     my $msg = $lifx->next_message(1);
@@ -16,7 +24,8 @@ while(1) {
         my $mac = $msg->bulb_mac();
         $bulb = $lifx->get_bulb_by_mac($mac);
         if (defined($bulb)) {
-            print $bulb->label(),"\n";
+            $mac = mac2str($mac);
+            print $bulb->label(),"($mac)\n";
             my $t = localtime($bulb->last_seen());
         } elsif ($mac eq ALL_BULBS) {
             print "All Bulbs\n";
